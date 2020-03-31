42b161a907d2d41cfbe6168b8bd4b8ae4f5d210e
Brandon J <splintercel3000@gmail.com>
2020-03-31 13:03:25 -0700

    stopping for now.

--- Lab11-TimePerson/Lab11-TimePerson/Models/TimePerson.cs
+++ Lab11-TimePerson/Lab11-TimePerson/Models/TimePerson.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,15 +23,56 @@ namespace Lab11_TimePerson.Models
         public static List<TimePerson> GetPersons(int begYear, int endYear)
         {
             // create a list of Time persons (instantiate a new list)
+            List<TimePerson> mylist = new List<TimePerson>();
+
             // get the path of your timeperson.csv file
             // getting the path is not as simple as ../../../ have to use 
             // the Path or Environment class
-
             // once you get the file path, 
             // read all the lines and save it into an array of strings
+
+            string stuff = "./wwwroot/personOfTheYear.csv";
+
+
+            /*  string[] stuffSplit = stuff[0].Split(',');*/
+
+            using (TextFieldParser csvParse = new TextFieldParser(stuff)) {
+                csvParse.CommentTokens = new string[] { "#" };
+                csvParse.SetDelimiters(new string [] { ","});
+                csvParse.HasFieldsEnclosedInQuotes = true;
+
+                csvParse.ReadLine();
+
+                while (!csvParse.EndOfData)
+                {
+                    string[] things = csvParse.ReadFields();
+                    string year = things[0];
+                    string honor = things[1];
+                    string name = things[2];
+                    string country = things[3];
+                    string birthyear = things[4];
+                    string deathyear = things[5];
+                    string title = things[6];
+                    string category = things[7];
+                    string context = things[8];
+
+                }
+            }
+
             // traverse through the strings for each line item
             // remember CSV is delimited by commas. 
-
+           /* TimePerson thing = new TimePerson()
+            {
+                Year = year,
+                Honor = honor,
+                Name = name,
+                Country = country,
+                BirthYear = birthyear,
+                DeathYear = deathyear,
+                Title = title,
+                Category = category,
+                Context = context;
+            }*/
             // use LINQ to filter out with the years that you brought in against your list of persons
 
             //return your list of persons
